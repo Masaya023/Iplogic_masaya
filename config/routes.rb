2595c772f4a777b5a8beebb336ccea69ac8d0e01
Masaya023 <sunrisesagain@outlook.jp>
2017-11-21 11:23:32 +0000

    last

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,11 @@
 Rails.application.routes.draw do
  
   # get '/blogs' => 'blogs#index'  #get 'URL'
-  #指定されたURLが入力されたら　=> ○○ controller＃アクション
- 
-  resources :blogs
-
+  #指定されたURLが入力されたら => ○○ controller＃アクション
   root 'blog#index'
-
-
-
+  resources :blogs
 end
 
 
 # URL -> routes -> controller -> #アクション -> html
-#リクエスト　　　　　　　　　　　blogs　　　　　　index　　　  リスポンス
+#リクエスト              blogs        index     レスポンス
